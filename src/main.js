c1c741bcc2367a3c1774fb54ee3e0fd4c4f2f5aa
Daniel Figueiredo <necoraio@hotmail.com>
2024-11-14 17:29:59 -0300

    first configs webpack

---
+++ src/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import './styles/global.css';
+import './styles/header.css';
+import './styles/main.css';
+import './styles/date-appointments.css';
+import './styles/list.css';
+import './styles/morning-appointments.css';
+import './styles/afternoon-appointments.css';
+import './styles/night-appointments.css';
+import './styles/button.css';
+import './styles/footer.css';
+import './styles/./modal/index.css';
+import './styles/utility.css';
+import './styles/modal/index.css';
+import './styles/modal/input.css';
